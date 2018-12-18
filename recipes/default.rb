86407abaef17acd723b454829867283b0216a4fb
Ravindra44 <ravihadoop9@gmail.com>
2018-12-18 17:13:53 +0530

    changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# install apache 
+package "httpd" do
+	action :install
+end
+# apache service is enabled to start on reboot 
+service "httpd" do
+	action [:enable, :start]
+end
+cookbook_file "/var/www/html/index.html" do 
+	source "index.html"
+	mode "0644"
+end
